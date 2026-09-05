098b4ed16c9b1245213e76d17eaddd41ec6c1fff
AdelMini <AdelMini@BEELINKMINIS>
2026-09-06 00:02:11 +0300

    Добавьте файлы проекта.

---
+++ dz_from_Latypova/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace dz_from_Latypova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Задание 1:");
+            //float num_e = 2.7f;
+            //Console.WriteLine("число е: " + num_e);
+            Console.Write("введите ваше имя: ");
+            string name =  Console.ReadLine();
+            Console.WriteLine($"привет {name}!");
+        }
+    }
+}
